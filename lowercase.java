5ba742ca55913e1780f5a86b2a8b17d4bf05abc9
divyaramya <sridivyaganesan97@gmail.com>
2017-06-26 15:17:33 +0530

    Create lowercase.java

---
+++ lowercase.java
@@ -0,0 +1,15 @@
+
+public class JavaProgram
+{
+    public static void main(String[] input)
+    {
+        String strUpper, strLower;
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Enter one word/name in Uppercase : ");
+        strUpper = scan.nextLine();
+        
+        strLower = strUpper.toLowerCase();
+        System.out.print("Equivalent Word/Name in Lowercase : " + strLower);
+    }
+}
